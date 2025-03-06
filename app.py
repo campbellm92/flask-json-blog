ad176078e4e1d8e4b011c0c7e251665d7df626da
Matthew Campbell <m.campbell92@gmail.com>
2025-03-06 21:09:12 +0100

    begin config of json file loader

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template, json
-
+from flask import Flask, render_template, request, jsonify
+import json
 
 app = Flask(__name__)
 
+with open('articles.json', 'r') as f:
+    articles = json.load(f)
+
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('index.html', articles=articles)
 
 @app.route("/post")
 def post():
-    return render_template('post.html')
+    return render_template('post.html', articles=articles)
