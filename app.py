9bb7e50a8556d2b5cca612f86cf6f884b13ebe01
Matthew Campbell <m.campbell92@gmail.com>
2025-03-06 22:21:37 +0100

    introduce logic for rendering single articles, import newline func

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, jsonify
 import json
+from flask import Flask, render_template, request, jsonify, abort
+from utils.jinja_filters import newline_to_break
 
 app = Flask(__name__)
+app.jinja_env.filters['newline_to_break'] = newline_to_break
 
 with open('articles.json', 'r') as f:
     articles = json.load(f)
@@ -10,6 +12,9 @@ with open('articles.json', 'r') as f:
 def home():
     return render_template('index.html', articles=articles)
 
-@app.route("/post")
-def post():
-    return render_template('post.html', articles=articles)
+@app.route("/post/<article_id>")
+def get_article(article_id):
+    article = next((a for a in articles if a["id"] == article_id), None)
+    if not article:
+        abort(404)
+    return render_template('post.html', article=article)
